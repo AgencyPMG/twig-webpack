9488e5defd4996d3902c6ca900465d676fc614b6
Christopher Davis <cdavis9999@gmail.com>
2016-07-13 18:44:25 -0400

    Make the DI Factory Actually Work
    Not dependent on the order bundles are loaded.

--- src/Bundle/DependencyInjection/PmgTwigWebpackExtension.php
+++ src/Bundle/DependencyInjection/PmgTwigWebpackExtension.php
@@ -13,25 +13,31 @@ namespace PMG\TwigWebpack\Bundle\DependencyInjection;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\Asset\Exception\InvalidArgumentException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use PMG\TwigWebpack\SimpleWebpack;
 use PMG\TwigWebpack\SymfonyAssetsWebpack;
+use PMG\TwigWebpack\Webpack;
 use PMG\TwigWebpack\Twig\WebpackExtension;
 
 final class PmgTwigWebpackExtension extends ConfigurableExtension
 {
-    public static function createAssetsWebpack(Packages $packages, $devMode, $baseUrl)
+    public static function createWebpack(Packages $packages=null, $devMode, $baseUrl)
     {
+        if (!$packages) {
+            return new SimpleWebpack($devModek, $baseUrl);
+        }
+
         try {
-            $package = $package->getPackage('webpack');
+            $package = $packages->getPackage('webpack');
         } catch (InvalidArgumentException $e) {
             $package = SymfonyAssetsWebpack::createPackage($baseUrl);
             $packages->addPackage('webpack', $package);
         }
 
-        return new SymfonyAssetsWebpack($devMode, $packge);
+        return new SymfonyAssetsWebpack($devMode, $package);
     }
 
     /**
@@ -43,33 +49,16 @@ final class PmgTwigWebpackExtension extends ConfigurableExtension
             $container->setParameter(sprintf('pmg_twig_webpack.%s', $key), $val);
         }
 
-        if ($container->hasDefinition('assets.packages')) {
-            $webpack = $this->createAssetsWebpack();
-        } else {
-            $webpack = $this->createSimpleWebpack($config);
-        }
-        $container->setDefinition('pmg_twig_webpack.webpack', $webpack);
+        $container->setDefinition('pmg_twig_webpack.webpack', new Definition(Webpack::class, [
+                new Reference('assets.packages', ContainerInterface::NULL_ON_INVALID_REFERENCE),
+                '%pmg_twig_webpack.dev_mode%',
+                '%pmg_twig_webpack.base_url%',
+            ]))
+            ->setFactory([__CLASS__, 'createWebpack']);
 
         $container->setDefinition('pmg_twig_webpack.extension', new Definition(
             WebpackExtension::class,
             [new Reference('pmg_twig_webpack.webpack')]
         ))->addTag('twig.extension');
-    }
-
-    private function assetsWebpackDefinition()
-    {
-        $def = new Definition(SymfonyAssetsWebpack::class, [
-            '%pmg_twig_webpack.dev_mode%',
-            '%pmg_twig_webpack.base_url%',
-            new Reference('assets.packages'),
-        ]);
-        $def->setFactory([__CLASS__, 'createAssetsWebpack']);
-
-        return $def;
-    }
-
-    private function simpleWebpackDefinition(array $config)
-    {
-        return new Definition(SimpleWebpack::class, [$config['dev_mode'], $config['base_url']]);
     }
 }
